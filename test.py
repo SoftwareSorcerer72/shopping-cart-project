f0e25944438e4974e20747d466e50179fd622b53
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-04 04:32:37 -0500

    added comments explaining the one bug i found, but its not a deal breaker and jsut makes 'item not found' show up after an item is removed until the next action.

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ def get_int(prompt):
 def run_program(): 
     shopping_cart = {}
     run_program = '0'
-    while run_program != '5':
-        run_program = input('Welcome to Mike\'s shopping cart app. What would you like to do?\n Remember when entering a price or a quantity use the \"0.00\" format!\n\t To add an item type 1\n\t To remove an item type 2\n\t To view all items in your cart type 3 \n\t To update an item quantity/price type 4\n\t To exit type 5\n')
+    while run_program != '4':
+        run_program = input('Welcome to Mike\'s shopping cart app. What would you like to do?\n Remember when entering a price or a quantity use the \"0.00\" format!\n\t To add an item type 1\n\t To remove an item type 2\n\t To update an item quantity/price type 3\n\t To exit type 4\n')
         while not run_program.isdigit():
             run_program = input('Please enter a number')
 
@@ -40,10 +40,6 @@ def run_program():
                 print('Item not found.')
 
         elif run_program == '3':
-            for name, info in shopping_cart.items():
-                print('Name: ', name, ', Price: ', info['price'], ', Quantity: ', info['quantity'])
-
-        elif run_program == '4':
             name = input('Enter the name of the item to update: ').lower().title()
             if name in shopping_cart:
                 new_price = get_float('Enter the new price: ')
@@ -60,3 +56,5 @@ def run_program():
         print(f'Item: {name}, Price: {info["price"]}, Quantity: {info["quantity"]}')
 
 run_program()
+
+# I'm happy with this overall. There is one small minor bug, and that is after an item is deleted it says item not found in the dictionary when it updates the cart initially, but then it goes away after the next action. I'm not sure why that is happening. Probably something to do with an if, elif, or else statement. I'll have to look into that.
