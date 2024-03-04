ecd74a10caa94b91ef65daf4960bc8ebf9dab265
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-04 04:09:03 -0500

    I converted my address book over and added a dictionary to my dictionary to get it to hold the aditional values of quantity and price while still being attached to teh item name. I also set up this nifty function that makes it so the price and quantity are checked to see if they are integers or floats. However it might break my code lol because I've never used except or try.

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-shopping_cart = {}
+def get_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("That's not a valid number! Please try again.")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("That's not a valid number! Please try again.")
 
 def run_program(): 
+    shopping_cart = {}
     run_program = '0'
     while run_program != '5':
         run_program = input('What would you like to do?\n\t To add an item type 1\n\t To remove an item type 2\n\t To view all items type 3 \n\t To update an item quantity/price type 4\n\t To exit type 5\n')
@@ -9,8 +22,8 @@ def run_program():
 
         if run_program == '1':
             name = input('Enter the name of the item you wish to add: ').lower().title()
-            price = float(input('Enter the price: '))
-            quantity = int(input('Enter the quantity: '))
+            price = get_float('Enter the price: ')
+            quantity = get_int('Enter the quantity: ')
             shopping_cart[name] = {'price': price, 'quantity': quantity}
 
         elif run_program == '2':
@@ -27,8 +40,8 @@ def run_program():
         elif run_program == '4':
             name = input('Enter the name of the item to update: ').lower().title()
             if name in shopping_cart:
-                new_price = float(input('Enter the new price: '))
-                new_quantity = int(input('Enter the new quantity: '))
+                new_price = get_float('Enter the new price: ')
+                new_quantity = get_int('Enter the new quantity: ')
                 shopping_cart[name] = {'price': new_price, 'quantity': new_quantity}
             else:
                 print('Item not found.')
