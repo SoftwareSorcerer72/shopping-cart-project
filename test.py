39a2626b0b7ae87bba48d73236b475b1a608c380
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-04 04:25:04 -0500

    I repicated the cart message a couple of time through the program. Now im wondering though if option 3 to see the cart is redundant. gonna cpush and then try to remove it without breaking anything.

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def run_program():
     shopping_cart = {}
     run_program = '0'
     while run_program != '5':
-        run_program = input('Welcome to Mike\'s shopping cart app. What would you like to do?\n Remeber when entering a price or a quantity use the \"0.00\" format!\n\t To add an item type 1\n\t To remove an item type 2\n\t To view all items in your cart type 3 \n\t To update an item quantity/price type 4\n\t To exit type 5\n')
+        run_program = input('Welcome to Mike\'s shopping cart app. What would you like to do?\n Remember when entering a price or a quantity use the \"0.00\" format!\n\t To add an item type 1\n\t To remove an item type 2\n\t To view all items in your cart type 3 \n\t To update an item quantity/price type 4\n\t To exit type 5\n')
         while not run_program.isdigit():
             run_program = input('Please enter a number')
 
@@ -25,11 +25,17 @@ def run_program():
             price = get_float('Enter the price: ')
             quantity = get_int('Enter the quantity: ')
             shopping_cart[name] = {'price': price, 'quantity': quantity}
+            print('I\'ve updated your cart: ')
+            for name, info in shopping_cart.items():
+                print(f'Item: {name}, Price: {info["price"]}, Quantity: {info["quantity"]}')
 
         elif run_program == '2':
             name = input('Enter the name of the item to remove: ').lower().title()
             if name in shopping_cart:
                 del shopping_cart[name]
+                print('I\'ve updated your cart: ')
+            for name, info in shopping_cart.items():
+                print(f'Item: {name}, Price: {info["price"]}, Quantity: {info["quantity"]}')
             else:
                 print('Item not found.')
 
@@ -43,6 +49,9 @@ def run_program():
                 new_price = get_float('Enter the new price: ')
                 new_quantity = get_int('Enter the new quantity: ')
                 shopping_cart[name] = {'price': new_price, 'quantity': new_quantity}
+                print('I\'ve updated your cart: ')
+            for name, info in shopping_cart.items():
+                print(f'Item: {name}, Price: {info["price"]}, Quantity: {info["quantity"]}')
             else:
                 print('Item not found.')
 
